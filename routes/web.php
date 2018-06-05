a9d99130e44db3ebde471d7d893f5861425bdeed
Selah Raquiño <keijiroupacheco@gmail.com>
2018-06-05 15:00:04 +0800

    changed web

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,6 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::get('/bincard', 'MSIController@bincard');
 	Route::post('/addbincard', 'MSIController@addbincard');
 	Route::post('/addbincardcontent', 'MSIController@addbincardcontent');
-<<<<<<< HEAD
-=======
-
->>>>>>> 1d6144e0782a02082a202c1229e7328ab14101eb
-	Route::get('/viewbincard/{id}', 'MSIController@viewbincard');
-	Route::get('/stockcard', 'MSIController@stockcard');
-	Route::post('/addstockcard', 'MSIController@addstockcard');
-	Route::post('/addstockcardcontent', 'MSIController@addstockcardcontent');
-	Route::get('/viewstockcard/{id}', 'MSIController@viewstockcard');
-	Route::get('/viewstockstatus', 'MSIController@viewstockstatus');
-	Route::get('/MRS', 'MSIController@MRS');
-	Route::post('/addmrs', 'MSIController@addmrs');
-	Route::post('/addmrscontent', 'MSIController@addmrscontent');
-	Route::get('/viewmrs/{id}', 'MSIController@viewmrs');
-	Route::get('/disbursementvoucher', 'MSIController@disbursementvoucher');
-	Route::post('/adddisbursementvoucher', 'MSIController@adddisbursementvoucher');
-	Route::get('/viewdisbursementvoucher/{id}', 'MSIController@viewdisbursementvoucher');
-<<<<<<< HEAD
-=======
-
->>>>>>> 1d6144e0782a02082a202c1229e7328ab14101eb
 	Route::get('/viewbincard/{id}', 'MSIController@viewbincard')->where('x', '[0-9]+');;
 	Route::get('/stockcard', 'MSIController@stockcard');
 	Route::post('/addstockcard', 'MSIController@addstockcard');
@@ -43,8 +22,11 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::post('/addpurchase_requisition', 'MSIController@addpurchase_requisition');
 	Route::get('/viewpurchase_requisition/{id}', 'MSIController@viewpurchase_requisition');
 	Route::post('/addpurchase_requisitioncontent', 'MSIController@addpurchase_requisitioncontent');
-<<<<<<< HEAD
-=======
-
->>>>>>> 1d6144e0782a02082a202c1229e7328ab14101eb
+	Route::get('/MRS', 'MSIController@MRS');
+	Route::post('/addmrs', 'MSIController@addmrs');
+	Route::post('/addmrscontent', 'MSIController@addmrscontent');
+	Route::get('/viewmrs/{id}', 'MSIController@viewmrs');
+	Route::get('/disbursementvoucher', 'MSIController@disbursementvoucher');
+	Route::post('/adddisbursementvoucher', 'MSIController@adddisbursementvoucher');
+	Route::get('/viewdisbursementvoucher/{id}', 'MSIController@viewdisbursementvoucher');
 });
