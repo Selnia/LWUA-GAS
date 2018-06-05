98342a1c1fc20a3f9a36331fab799fba354521a7
Selah Raquiño <keijiroupacheco@gmail.com>
2018-06-05 14:52:38 +0800

    changed

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::get('/bincard', 'MSIController@bincard');
 	Route::post('/addbincard', 'MSIController@addbincard');
 	Route::post('/addbincardcontent', 'MSIController@addbincardcontent');
-<<<<<<< HEAD
 	Route::get('/viewbincard/{id}', 'MSIController@viewbincard');
 	Route::get('/stockcard', 'MSIController@stockcard');
 	Route::post('/addstockcard', 'MSIController@addstockcard');
@@ -19,7 +18,6 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::get('/disbursementvoucher', 'MSIController@disbursementvoucher');
 	Route::post('/adddisbursementvoucher', 'MSIController@adddisbursementvoucher');
 	Route::get('/viewdisbursementvoucher/{id}', 'MSIController@viewdisbursementvoucher');
-=======
 	Route::get('/viewbincard/{id}', 'MSIController@viewbincard')->where('x', '[0-9]+');;
 	Route::get('/stockcard', 'MSIController@stockcard');
 	Route::post('/addstockcard', 'MSIController@addstockcard');
@@ -37,6 +35,4 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::post('/addpurchase_requisition', 'MSIController@addpurchase_requisition');
 	Route::get('/viewpurchase_requisition/{id}', 'MSIController@viewpurchase_requisition');
 	Route::post('/addpurchase_requisitioncontent', 'MSIController@addpurchase_requisitioncontent');
-
->>>>>>> f259e4e1cd6bcad117be1d0e51fdd1575d453c32
 });
