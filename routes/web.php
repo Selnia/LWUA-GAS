7ba222049cae75342346a0238965627f4c13fb0d
Selah Raquiño <keijiroupacheco@gmail.com>
2018-06-08 16:28:15 +0800

    route adjsted

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,10 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::get('/viewstockcard/{id}', 'MSIController@viewstockcard')->where('x', '[0-9]+');
 	Route::get('/viewstockstatus', 'MSIController@viewstockstatus');
 	Route::group(['prefix' => 'viewinventory'], function () {
-		Route::get('/monthly/{x}', 'MSIController@viewinventorymonthly')->where('x', '([1-9]|1[0-2])');
-		Route::get('/quarterly/{x}', 'MSIController@viewinventoryquarterly')->where('x', '[1-4]');
-		Route::get('/semiannually/{x}', 'MSIController@viewinventorysemiannually')->where('x', '[1-2]');
-		Route::get('/annually/{x}', 'MSIController@viewinventoryannually')->where('x', '[1-2]');
+		Route::get('/monthly/{x}/{y}', 'MSIController@viewinventorymonthly')->where('x', '([1-9]|1[0-2])')->where('y', '[1-5]');
+		Route::get('/quarterly/{x}/{y}', 'MSIController@viewinventoryquarterly')->where('x', '[1-4]')->where('y', '[1-5]');
+		Route::get('/semiannually/{x}/{y}', 'MSIController@viewinventorysemiannually')->where('x', '[1-2]')->where('y', '[1-5]');
+		Route::get('/annually/{y}', 'MSIController@viewinventoryannually')->where('y', '[1-5]');
 	});
 	Route::get('/purchase_requisition', 'MSIController@purchase_requisition');
 	Route::post('/addpurchase_requisition', 'MSIController@addpurchase_requisition');
