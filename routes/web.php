a73e6cc7973eb4ff95ce8cad5ede8b81d65b78a8
Selah Raquiño <keijiroupacheco@gmail.com>
2018-06-08 16:31:05 +0800

    route adjusted

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::group(['prefix' => 'msi'], function () {
 	Route::post('/addstockcard', 'MSIController@addstockcard');
 	Route::post('/addstockcardcontent', 'MSIController@addstockcardcontent');
 	Route::get('/viewstockcard/{id}', 'MSIController@viewstockcard')->where('x', '[0-9]+');
-	Route::get('/viewstockstatus', 'MSIController@viewstockstatus');
+	Route::get('/viewstockstatus/{x}/{y}', 'MSIController@viewstockstatus')->where('x', '([1-9]|1[0-2])')->where('y', '[1-5]');
 	Route::group(['prefix' => 'viewinventory'], function () {
 		Route::get('/monthly/{x}/{y}', 'MSIController@viewinventorymonthly')->where('x', '([1-9]|1[0-2])')->where('y', '[1-5]');
 		Route::get('/quarterly/{x}/{y}', 'MSIController@viewinventoryquarterly')->where('x', '[1-4]')->where('y', '[1-5]');
